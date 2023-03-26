c463bbfb678f9dea7ab09c4ef9986afa8a6c1943
Aghogho <monorienaghogho@gmail.com>
2023-03-25 21:39:14 -0700

    4. Client config with Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,5 +5,5 @@ file { '/etc/ssh/ssh_config':
   owner   => 'root',
   group   => 'root',
   mode    => '0644',
-  content => template('/home/ubuntu/alx-system_engineering-devops/0x0B-ssh/ssh_config.erb'),
+  content => template('/etc/ssh/ssh_config'),
 }
