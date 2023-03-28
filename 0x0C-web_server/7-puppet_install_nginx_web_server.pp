26aed0812015bfb665c7b0563e853ab545f66293
Aghogho <monorienaghogoh@gmail.com>
2023-03-28 16:40:11 -0700

    5. install nginx web server with Puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,41 @@
+class nginx {
+  package { 'nginx':
+    ensure => 'installed',
+  }
+
+  service { 'nginx':
+    ensure => 'running',
+    enable => true,
+  }
+
+  file { '/etc/nginx/html/index.html':
+    content => "Hello World!\n",
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    content => "server {
+                  listen 80;
+                  root /etc/nginx/html;
+                  index index.html;
+                  location /redirect_me {
+                    return 301 https://www.google.com/;
+                  }
+                }",
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    notify  => Service['nginx'],
+  }
+
+  file { '/etc/nginx/sites-enabled/000-default':
+    ensure  => 'link',
+    target  => '/etc/nginx/sites-available/default',
+    require => File['/etc/nginx/sites-available/default'],
+  }
+}
+
+include nginx
+
