4544e7dfabbca49b65dfd9b18df84bfe4a2643f7
Aghogho <monorienaghogho@gmail.com>
2023-04-24 14:25:48 -0700

    0. gather data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """TODO REST API"""
-import urllib.request
 import json
 import sys
+import urllib.request
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user_request = urllib.request.urlopen(url + "users/{}".format(sys.argv[1]))
     user = json.loads(user_request.read().decode())
-    todos_request = urllib.request.urlopen(url + "todos?userId={}".format(sys.argv[1]))
+    url_todos = url + "todos?userId={}"
+    todos_request = urllib.request.urlopen(url_todos.format(sys.argv[1]))
     todos = json.loads(todos_request.read().decode())
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
