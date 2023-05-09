d003f1814219b07dd13c2ad57c75010c50c83568
aghogho <aghoghomonorien@gmail.com>
2023-05-10 00:31:07 +0100

    1. top 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -16,3 +16,4 @@ def top_ten(subreddit):
         [print(child.get("data").get("title")) for child in results.get("children")]
     except requests.exceptions.JSONDecodeError:
         print(None)
+    return
