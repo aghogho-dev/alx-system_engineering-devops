f8354735a5ae33d3e8b45cdf8193f5d9d6a95e44
aghogho <aghoghomonorien@gmail.com>
2023-05-10 00:29:48 +0100

    1. top 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,10 +11,8 @@ def top_ten(subreddit):
     response = requests.get(url, headers=headers, params=params, allow_redirects=False)
     if response.status_code == 404:
         print("None")
-        return None
     try:
         results = response.json().get("data")
         [print(child.get("data").get("title")) for child in results.get("children")]
     except requests.exceptions.JSONDecodeError:
         print(None)
-        return None
