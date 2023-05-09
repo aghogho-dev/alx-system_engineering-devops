29d87f604786ec01ecab2c2c87e20dddb465f05a
aghogho <aghoghomonorien@gmail.com>
2023-05-10 00:27:54 +0100

    1. top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Get Top 10"""
+import requests
+
+
+def top_ten(subreddit):
+    """Get Top 10"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "MyBot/0.0.1"}
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return None
+    try:
+        results = response.json().get("data")
+        [print(child.get("data").get("title")) for child in results.get("children")]
+    except requests.exceptions.JSONDecodeError:
+        print(None)
+        return None
