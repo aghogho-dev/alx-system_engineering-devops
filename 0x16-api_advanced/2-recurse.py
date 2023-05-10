14b7549aab87183c08f74ec22f0359828733ba93
aghogho <aghoghomonorien@gmail.com>
2023-05-10 01:05:58 +0100

    2. recurse it

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,23 +3,23 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[], after="", count=0):
     """Recursive to get hot posts"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "MyBot/0.0.1"}
-    params = {"limit": 100, "after": after}
+    params = {"limit": 100, "after": after, "count": count}
     response = requests.get(url, headers=headers, params=params, allow_redirects=False)
     if response.status_code == 404:
         return None
     try:
         data = response.json().get("data")
-        if not data or not data.get("children"):
-            return hot_list
+        after = data.get("after")
+        count += data.get("dist")
         for child in data.get("children"):
             hot_list.append(child.get("data").get("title"))
-        after = data.get("after")
-        if not after:
-            return hot_list
-        return recurse(subreddit, hot_list, after)
+
+        if after is not None:
+            return recurse(subreddit, hot_list, after, count)
     except requests.exceptions.JSONDecodeError:
         return None
+    return hot_list
