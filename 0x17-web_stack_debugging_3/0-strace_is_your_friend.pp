f7f3e09bac02e5b4d3fc0ac9cf2b2d45d6279104
aghogho <aghoghomonorien@gmail.com>
2023-05-10 21:21:49 +0100

    0. strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,6 @@
 # Define an exec resource to fix the WordPress issue
 
-
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/',
-  onlyif  => '/usr/bin/grep -q "phpp" /var/www/html/wp-settings.php',
-  require => File['/var/www/html/wp-settings.php'],
+  path    => '/usr/local/bin/:/bin/'
 }
