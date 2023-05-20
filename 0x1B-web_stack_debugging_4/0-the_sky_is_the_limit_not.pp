793861cd043e438a1108e0f63ee9d3bf736a1cfb
aghogho <aghoghomonorien@gmail.com>
2023-05-20 04:23:37 +0100

    0. no limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Sky is the limit
 
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/worker_connections\s*\(.*\)/worker_connections 2000;/g" /etc/nginx/nginx.conf',
+  command => 'sed -i "s/worker_connections\s*\(.*\)/worker_connections 5000;/g" /etc/nginx/nginx.conf',
   path    => '/bin:/usr/bin',
-} ->
+}
 
 # Restart Nginx
 exec { 'nginx-restart':
